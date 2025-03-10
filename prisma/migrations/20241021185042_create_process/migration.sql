9005b5d166e2d1650f8a030c5c7508f1acfde580
Rafael-Reis1 <rafaelreis.12@hotmail.com>
2025-03-10 14:26:46 -0300

    Migra para o sqlserver

--- prisma/migrations/20241021185042_create_process/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Process" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "processInstanceId" INTEGER NOT NULL,
-    "processId" TEXT NOT NULL,
-    "userId" TEXT NOT NULL,
-    CONSTRAINT "Process_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
-);
