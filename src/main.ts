bf587e0322e9aa2d19aea476a23b97825a95fec0
Rafael-Reis1 <rafaelreis.12@hotmail.com>
2025-04-07 17:53:16 -0300

    Volta com o https

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as fs from 'fs';
+
+const httpsOptions = {
+  key: fs.readFileSync('./secrets/privadakey36994.key'),
+  cert: fs.readFileSync('./secrets/certificadopem36994.pem'),
+};
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   app.enableCors();
 
   // Pipes
