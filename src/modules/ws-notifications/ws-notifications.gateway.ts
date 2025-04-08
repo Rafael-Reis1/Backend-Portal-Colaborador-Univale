b6e4a1b8b79c074fc88887f026dcd4027543416b
Rafael-Reis1 <rafaelreis.12@hotmail.com>
2025-04-08 13:47:32 -0300

    Atualiza origins

--- src/modules/ws-notifications/ws-notifications.gateway.ts
+++ src/modules/ws-notifications/ws-notifications.gateway.ts
@@ -11,7 +11,7 @@ import { instrument } from '@socket.io/admin-ui';
 
 @WebSocketGateway({
   cors: {
-    origin: ["https://admin.socket.io", "https://portalcolaborador.univale.br/api/", "http://127.0.0.1:3000"],
+    origin: ["https://admin.socket.io", "https://portalcolaborador.univale.br/"],
     credentials: true
   }
 })
